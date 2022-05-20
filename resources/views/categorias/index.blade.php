e796b124f3e3e1b49d846ff148ce320009338be2
EduardoBarrAgu <barrancoaguilareduardo@gmail.com>
2022-05-19 22:05:22 -0500

    1.1

--- resources/views/categorias/index.blade.php
+++ resources/views/categorias/index.blade.php
@@ -1,59 +1,33 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        <meta name="description" content="" />
-        <meta name="author" content="" />
-        <title>Mercado</title>
-        <!-- Favicon-->
-        <link rel="icon" type="image/x-icon" href="{{ asset('assets/favicon_home.ico') }}" />
-        <!-- Core theme CSS (includes Bootstrap)-->
-        <link href="{{ asset('css/home.css') }}" rel="stylesheet" />
-    </head>
-    <body>
-        <!-- Responsive navbar-->
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <div class="container">
-                <a class="navbar-brand" href="{{ route('home') }}">Mercado</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
-                @include('widgets.search')
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                        <li class="nav-item"><a class="nav-link" href="">Log in</a></li>
-                        <li class="nav-item"><a class="nav-link" href="">Sign up</a></li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-        <!-- Page content-->
-        <div class="container">
-            <!-- Side widgets-->
-            <div class="col-lg-12">
-                <!-- Categories widget-->
-                <div class="card mb-12">
-                    <div class="card-header">Categorias</div>
-                    <div class="card-body">
-                        <div class="row">
-                          <div class="col-sm-2">
-                              <ul class="list-unstyled mb-0">
-                                  @foreach ($categorias as $categoria)
-                                  <li><a href="{{ route('home') }}?categoria_id={{ $categoria->id }}">{{ $categoria->nombre }}</a></li>
-                                  @endforeach
-                              </ul>
-                          </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- Footer-->
-        <footer class="py-5 bg-dark">
-            <div class="container"><p class="m-0 text-center text-white">S8A Escalera - Barranco - Martinez &copy; Mercado 2022</p></div>
-        </footer>
-        <!-- Bootstrap core JS-->
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
-        <!-- Core theme JS-->
-        <script src="{{ asset('js/scripts_home.js') }}"></script>
-    </body>
-</html>
+@extends('layout.base')
+
+@section('head')
+        <link href="{{ asset('css/inicio.css') }}" rel="stylesheet" />
+@endsection
+
+@section('content')
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Nombre</th>
+          <th scope="col">Activa</th>
+          <th scope="col"># Productos</th>
+          <th scope="col">Acciones</th>
+        </tr>
+      </thead>
+      <tbody>
+        @foreach($categorias as $categoria)
+            <tr>
+              <th scope="row">{{ $categoria->id }}</th>
+              <td>{{ $categoria->nombre }}</td>
+              <td>{{ $categoria->activa }}</td>
+              <td>{{ $categoria->productos_count }}</td>
+              <td>
+                <a href="{{ route('', [$categoria->id]) }}" class="btn btn-">Editar</a>
+                <a href="{{ route('', [$categoria->id]) }}" class="btn btn-danger">Eliminar</a>
+              </td>
+            </tr>
+        @endforeach
+      </tbody>
+    </table>
+@endsection
